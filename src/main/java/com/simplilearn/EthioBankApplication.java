3749af0fe92675ae4688298d8767277a5f06e7d5
zulikha <zshafi01@gmail.com>
2021-05-16 15:04:55 -0500

    first post

---
+++ src/main/java/com/simplilearn/EthioBankApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EthioBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EthioBankApplication.class, args);
+	}
+
+}
